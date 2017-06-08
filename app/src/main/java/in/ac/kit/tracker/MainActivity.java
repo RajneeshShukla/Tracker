c782ffc6a6fa34481cbd7b10533756c369d11797
Rajneesh <rajneesh9651@gmail.com>
2017-06-08 09:11:45 +0530

    Add Drawerlayout with daump data

--- app/src/main/java/in/ac/kit/tracker/MainActivity.java
+++ app/src/main/java/in/ac/kit/tracker/MainActivity.java
@@ -1,13 +1,43 @@
 package in.ac.kit.tracker;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    public DrawerLayout mDrawerlayout;
+    private ActionBarDrawerToggle mToggle;
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerlayout = (DrawerLayout) findViewById(R.id.Drawerlaout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerlayout, R.string.open, R.string.close);
+
+        //Addind toolbar
+        mToolbar = (Toolbar)findViewById(R.id.nav_actionbar);
+        setSupportActionBar(mToolbar);
+
+        mDrawerlayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+     //When hamburger icon is Clicked
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
